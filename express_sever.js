83bf7f03427385f9379b39e6e9c897b3ae9a6100
TedP <ted.pampilon@gmail.com>
2021-03-16 02:47:28 +0000

    made _header.ejs partial and included it in Urls_index.ejs and urls_show.ejs

--- express_sever.js
+++ express_sever.js
@@ -15,7 +15,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: 'http://www.lighthouselabs.ca' };
+  const templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
   res.render("urls_show", templateVars);
 });
 
