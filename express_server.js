f983b3d7e772e97b8bfb46b59c790cac91b013f6
TedP <ted.pampilon@gmail.com>
2021-03-16 18:16:07 +0000

    added morgan middleware

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const morgan = require('morgan')
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 
-const bodyParser = require("body-parser");
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 let urlDatabase = {
